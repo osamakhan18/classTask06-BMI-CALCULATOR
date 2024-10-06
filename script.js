b923d9be68703ce1a3e3e136c8cd5fc9314ebfcb
osamakhan18 <badishakhan18@gmail.com>
2024-10-06 06:33:06 -0700

    add the style for the main box and backgrounnd

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ var btn = document.getElementById('btn');
 btn.addEventListener('click',function(){
     var weight = document.getElementById('weight').value;
        var height = document.getElementById('height').value;
-    var bmi = weight/(height *2);
+    var bmi = weight/(height *height);
     
     var category;
     if(bmi<18.5){
         category = 'underweight'
     }
-  else   if(18.5>=bmi <=24.5){
-        catagory = 'normal'
+    else if(18.5>=bmi <=24.9){
+        category = 'normal'
     }
-    else if(25 <=bmi < 29.9){
+    else  if(25 <= bmi < 29.9){
         category = 'overweight'
     }
-    else(bmi>= 30)
+    else if (bmi  >= 30)
        category = 'obesity'
 
-    //    document.getElementById('result').innerText= `your bmi is ${category}`;
+   
     alert(`${category}`)
     
     
